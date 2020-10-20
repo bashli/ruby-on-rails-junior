8ce2327513a36f90f348e548a56cf1dd2573c3c5
Islam <islamgadzhiev0332@gmail.com>
2020-10-20 12:19:45 +0300

    My

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+
+  def index
+  	@posts = Post.all()
+
+  end
+
+  def topic
+  	@topic = Topic.find_by(alias: params[:topic])
+  	@posts = @topic.posts
+  	render 'index'
+  end	
+
+  def show
+  	@post = Post.find(params[:id])
+
+  end
+
+  def edit
+  	@topics = Topic.all()
+  	@post = Post.find(params[:id])
+
+  end
+
+  def new
+  	@topics = Topic.all() 
+  		
+  end
+
+  def create
+  	@post = Post.new(post_params)
+  	if @post.save
+  	redirect_to '/posts/' + @post.id.to_s
+  	else
+  	@topics = Topic.all()
+  	render 'new'
+  	end	
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+  	if @post.update(post_params)
+  	redirect_to '/posts/' + @post.id.to_s
+  	else
+  	@topics = Topic.all()
+  	render 'edit'
+  	end		
+  end
+
+  private
+  def post_params
+  	params.require(:post).permit(:title, :body, :topic_id) 		
+  end	
+
+end
